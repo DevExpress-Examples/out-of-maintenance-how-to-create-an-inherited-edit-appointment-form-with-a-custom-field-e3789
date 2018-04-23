a966396389212905a5f073371fbe852def8061b3
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerInheritedAppointmentForm {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            schedulerControl1.ActiveViewType = SchedulerViewType.Timeline;
+            schedulerControl1.GroupType = SchedulerGroupType.Resource;
+            schedulerControl1.TimelineView.ResourcesPerPage = 3;
+
+            schedulerControl1.InitNewAppointment += new AppointmentEventHandler(schedulerControl1_InitNewAppointment);
+        }
+
+        void schedulerControl1_InitNewAppointment(object sender, AppointmentEventArgs e) {
+            e.Appointment.CustomFields["Field1"] = 20;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+            this.carsTableAdapter.Fill(this.carsDBDataSet.Cars);
+            // TODO: This line of code loads data into the 'carsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
+            this.carSchedulingTableAdapter.Fill(this.carsDBDataSet.CarScheduling);
+
+            this.carSchedulingTableAdapter.Adapter.RowUpdated +=
+                new System.Data.OleDb.OleDbRowUpdatedEventHandler(Adapter_RowUpdated);
+
+            if (schedulerStorage1.Appointments.Count > 0)
+                schedulerControl1.Start = schedulerStorage1.Appointments[0].Start;
+        }
+
+        private void AppointmentsModified(object sender, DevExpress.XtraScheduler.PersistentObjectsEventArgs e) {
+            this.carSchedulingTableAdapter.Update(this.carsDBDataSet);
+            this.carsDBDataSet.AcceptChanges();
+        }
+
+        void Adapter_RowUpdated(object sender, System.Data.OleDb.OleDbRowUpdatedEventArgs e) {
+            if (e.Status == UpdateStatus.Continue && e.StatementType == StatementType.Insert) {
+                int id = 0;
+                using (OleDbCommand cmd = new OleDbCommand("SELECT @@IDENTITY", this.carSchedulingTableAdapter.Connection)) {
+                    id = (int)cmd.ExecuteScalar();
+                }
+                e.Row["ID"] = id;
+            }
+        }
+
+        private void schedulerControl1_EditAppointmentFormShowing(object sender, AppointmentFormEventArgs e) {
+            Appointment apt = e.Appointment;
+
+            // Required to open the recurrence form via context menu.
+            bool openRecurrenceForm = apt.IsRecurring && schedulerStorage1.Appointments.IsNewAppointment(apt);
+
+            CustomAppointmentForm customForm = new CustomAppointmentForm(schedulerControl1, apt, openRecurrenceForm);
+
+            customForm.LookAndFeel.ParentLookAndFeel = schedulerControl1.LookAndFeel;
+
+            e.DialogResult = customForm.ShowDialog();
+            schedulerControl1.Refresh();
+            e.Handled = true;
+        }
+    }
+}
---
+++ VB/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿Namespace SchedulerInheritedAppointmentForm
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim timeRuler1 As New DevExpress.XtraScheduler.TimeRuler()
+            Dim timeRuler2 As New DevExpress.XtraScheduler.TimeRuler()
+            Me.schedulerControl1 = New DevExpress.XtraScheduler.SchedulerControl()
+            Me.schedulerStorage1 = New DevExpress.XtraScheduler.SchedulerStorage(Me.components)
+            Me.carSchedulingBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.carsDBDataSet_Renamed = New SchedulerInheritedAppointmentForm.CarsDBDataSet()
+            Me.carsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.carSchedulingTableAdapter = New SchedulerInheritedAppointmentForm.CarsDBDataSetTableAdapters.CarSchedulingTableAdapter()
+            Me.carsTableAdapter = New SchedulerInheritedAppointmentForm.CarsDBDataSetTableAdapters.CarsTableAdapter()
+            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.carSchedulingBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.carsDBDataSet_Renamed, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.carsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' schedulerControl1
+            ' 
+            Me.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.schedulerControl1.Location = New System.Drawing.Point(0, 0)
+            Me.schedulerControl1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+            Me.schedulerControl1.Name = "schedulerControl1"
+            Me.schedulerControl1.OptionsView.ResourceHeaders.Height = 100
+            Me.schedulerControl1.OptionsView.ResourceHeaders.ImageAlign = DevExpress.XtraScheduler.HeaderImageAlign.Top
+            Me.schedulerControl1.OptionsView.ResourceHeaders.ImageSize = New System.Drawing.Size(100, 100)
+            Me.schedulerControl1.OptionsView.ResourceHeaders.ImageSizeMode = DevExpress.XtraScheduler.HeaderImageSizeMode.ZoomImage
+            Me.schedulerControl1.Size = New System.Drawing.Size(897, 482)
+            Me.schedulerControl1.Start = New Date(2008, 10, 24, 0, 0, 0, 0)
+            Me.schedulerControl1.Storage = Me.schedulerStorage1
+            Me.schedulerControl1.TabIndex = 0
+            Me.schedulerControl1.Text = "schedulerControl1"
+            Me.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1)
+            Me.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2)
+            ' 
+            ' schedulerStorage1
+            ' 
+            Me.schedulerStorage1.Appointments.CustomFieldMappings.Add(New DevExpress.XtraScheduler.AppointmentCustomFieldMapping("Field1", "Price"))
+            Me.schedulerStorage1.Appointments.DataSource = Me.carSchedulingBindingSource
+            Me.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay"
+            Me.schedulerStorage1.Appointments.Mappings.Description = "Description"
+            Me.schedulerStorage1.Appointments.Mappings.End = "EndTime"
+            Me.schedulerStorage1.Appointments.Mappings.Label = "Label"
+            Me.schedulerStorage1.Appointments.Mappings.Location = "Location"
+            Me.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo"
+            Me.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo"
+            Me.schedulerStorage1.Appointments.Mappings.ResourceId = "CarId"
+            Me.schedulerStorage1.Appointments.Mappings.Start = "StartTime"
+            Me.schedulerStorage1.Appointments.Mappings.Status = "Status"
+            Me.schedulerStorage1.Appointments.Mappings.Subject = "Subject"
+            Me.schedulerStorage1.Appointments.Mappings.Type = "EventType"
+            Me.schedulerStorage1.Resources.DataSource = Me.carsBindingSource
+            Me.schedulerStorage1.Resources.Mappings.Caption = "Model"
+            Me.schedulerStorage1.Resources.Mappings.Id = "ID"
+            Me.schedulerStorage1.Resources.Mappings.Image = "Picture"
+            ' 
+            ' carSchedulingBindingSource
+            ' 
+            Me.carSchedulingBindingSource.DataMember = "CarScheduling"
+            Me.carSchedulingBindingSource.DataSource = Me.carsDBDataSet_Renamed
+            ' 
+            ' carsDBDataSet
+            ' 
+            Me.carsDBDataSet_Renamed.DataSetName = "CarsDBDataSet"
+            Me.carsDBDataSet_Renamed.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' carsBindingSource
+            ' 
+            Me.carsBindingSource.DataMember = "Cars"
+            Me.carsBindingSource.DataSource = Me.carsDBDataSet_Renamed
+            ' 
+            ' carSchedulingTableAdapter
+            ' 
+            Me.carSchedulingTableAdapter.ClearBeforeFill = True
+            ' 
+            ' carsTableAdapter
+            ' 
+            Me.carsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(897, 482)
+            Me.Controls.Add(Me.schedulerControl1)
+            Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(Me.schedulerControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.schedulerStorage1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.carSchedulingBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.carsDBDataSet_Renamed, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.carsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private WithEvents schedulerControl1 As DevExpress.XtraScheduler.SchedulerControl
+
+        Private carsDBDataSet_Renamed As CarsDBDataSet
+        Private WithEvents schedulerStorage1 As DevExpress.XtraScheduler.SchedulerStorage
+        Private carSchedulingBindingSource As System.Windows.Forms.BindingSource
+        Private carSchedulingTableAdapter As SchedulerInheritedAppointmentForm.CarsDBDataSetTableAdapters.CarSchedulingTableAdapter
+        Private carsBindingSource As System.Windows.Forms.BindingSource
+        Private carsTableAdapter As SchedulerInheritedAppointmentForm.CarsDBDataSetTableAdapters.CarsTableAdapter
+    End Class
+End Namespace
+
